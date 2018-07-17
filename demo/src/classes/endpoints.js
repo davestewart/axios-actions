efadb841646fb49997ce92b6921856b7a48c4d50
Dave Stewart <dave@davestewart.co.uk>
2018-07-17 01:24:11 +0100

    Update demo files

--- demo/src/classes/endpoints.js
+++ demo/src/classes/endpoints.js
@@ -1,8 +1,29 @@
-import { Endpoint } from '../../../dist/axios-api.esm'
+import { Group, Endpoint, helpers } from '../../../dist/axios-actions.esm'
+
+export class Crud extends Endpoint {
+
+  constructor (axios, path, optimize, map) {
+    super(axios, path)
+
+    if (map) {
+      this.http.before.push(data => helpers.remap(data, map, false))
+      this.http.after.push(({ data }) => helpers.remap(data, map, true))
+    }
+
+    if (optimize) {
+      this.http.after.push(res => res.data)
+    }
+  }
+}
+
 
 function Post (data) {
   Object.keys(data)
-    .forEach(key => this[key] = `${key} > ${data[key]}`)
+    .forEach(key => this[key] = `[[ ${key} ]] ${data[key]}`)
+
+  this.alert = function () {
+    alert(this.t)
+  }
 }
 
 function transform (data, inbound, tx) {
@@ -13,10 +34,26 @@ export class Users extends Endpoint {
   constructor (axios) {
 
     const urls = {
-      read:   'User/GetUser',
-      create: 'User/CreateUser',
-      update: 'User/UpdateUser',
-      delete: 'User/DeleteUser',
+      read:   'POST User/GetUser',
+      create: 'POST User/CreateUser',
+      update: 'POST User/UpdateUser',
+      delete: 'POST User/DeleteUser',
+      refresh: 'GET User/Refresh'
+    }
+
+    super(axios, urls, false, true)
+  }
+}
+
+export class Comments extends Endpoint {
+  constructor (axios) {
+
+    const urls = {
+      browse: 'GET  comments',
+      read:   'GET  comments/:id',
+      create: 'POST comments/:id',
+      update: 'POST comments/:id',
+      delete: 'POST comments/:id',
     }
 
     super(axios, urls, false, true)
@@ -34,5 +71,15 @@ export class Posts extends Endpoint {
     }
 
     super(axios, 'posts/:id', true, map)
+
+    // convert to posts
+    this.http.after.push(data => helpers.process(data, obj => new Post(obj)))
+  }
+}
+
+export class Model extends Endpoint {
+  constructor (axios, path, classDef) {
+    super(axios, path, true)
+    this.http.after.push(data => helpers.process(data, obj => new classDef(obj)))
   }
 }
