d80f0cb706098adba2f69d134e1548646cd8db35
Dave Stewart <dave@davestewart.co.uk>
2018-07-21 17:39:22 +0100

    Update docs

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,27 +1,60 @@
 <template>
-  <div id="app" :class="endpoint.loading ? 'loading' : ''">
-    <section>
-      <button class="button" @click="browse()">Browse</button>
-      <button class="button" @click="read()">Read</button>
-      <button class="button" @click="create()">Create</button>
-      <button class="button" @click="update()">Update</button>
-      <button class="button" @click="remove()">Delete</button>
-      <button class="button" @click="users()">Users</button>
+  <main id="app" class="container">
+
+    <section class="section">
+      <h1 class="title">Axios Actions Demo</h1>
+      <div class="content">
+        <p><a href="http://github.com/davestewart/axios-actions">github.com/davestewart/axios-actions</a></p>
+      </div>
     </section>
-    <section>
-      <pre v-if="endpoint.error">{{ endpoint.error.message }}</pre>
-      <pre v-if="!endpoint.loading">{{ data }}</pre>
+
+    <section :class="endpoint.loading ? 'loading' : ''">
+
+      <section class="section">
+        <div class="control">
+          <button class="sm button" @click="index()">Index</button>
+          <button class="sm button" @click="search()">Search</button>
+          <button class="sm button" @click="read()">Read</button>
+          <button class="sm button" @click="create()">Create</button>
+          <button class="sm button" @click="update()">Update</button>
+          <button class="sm button" @click="remove()">Delete</button>
+        </div>
+      </section>
+
+      <section class="section">
+        <pre v-if="endpoint.error">{{ endpoint.error.message }}</pre>
+        <pre v-else-if="!endpoint.loading">{{ data }}</pre>
+      </section>
+
     </section>
-  </div>
+
+    <footer>
+      <p>JSON courtesy of <a href="https://jsonplaceholder.typicode.com/">jsonplaceholder.typicode.com</a></p>
+    </footer>
+
+  </main>
 </template>
 
 <script>
 import axios from 'axios'
-import { Endpoint } from '../../dist/axios-actions.esm'
+import { ApiEndpoint, plugins } from '../../dist/axios-actions.esm'
 
-const endpoint = new Endpoint(axios, 'posts/:id')
+function payload (data) {
+  return {
+    ...data,
+    title: 'New title @ ' + Date.now(),
+    body: 'New body @ ' + Date.now()
+  }
+}
 
-console.log(endpoint)
+// set up REST endpoint
+const posts = new ApiEndpoint(axios, 'posts/:id')
+
+// return data by default
+plugins.optimize(posts)
+
+// add a new endpoint
+posts.actions.add('search', 'GET posts?userId=:id')
 
 export default {
   name: 'app',
@@ -30,47 +63,42 @@ export default {
     return {
       data: null,
       error: null,
-      endpoint: endpoint
+      endpoint: posts
         .done(this.done)
         .fail(this.fail)
     }
   },
 
-  mounted () {
-    console.log(this.endpoint.done)
-    this.endpoint.map.add('users', 'users/:id')
-  },
-
   methods: {
 
-    browse () {
-      this.endpoint.browse().then(data => {
-        console.log('browsed!')
+    index () {
+      this.endpoint.index().then(data => {
+        console.log('Index loaded!')
       })
     },
 
     read () {
-      this.endpoint.read(50)
+      this.endpoint.read(10)
     },
 
     create () {
-      this.endpoint.create(this.payload())
+      this.endpoint.create(payload())
     },
 
     update () {
-      this.endpoint.update({ id: 45, ...this.payload() })
+      this.endpoint.update(payload({ id: 20 }))
     },
 
     remove () {
-      this.endpoint.delete(20)
+      this.endpoint.delete(30)
+    },
+
+    search () {
+      this.endpoint.call('search', 2)
     },
 
     done (data) {
       this.data = data
-    },
-
-    users () {
-      this.endpoint.call('get', 'users/:id', { id: 1 })
     },
 
     fail (error) {
@@ -79,23 +107,42 @@ export default {
       console.log(error)
     },
 
-    payload () {
-      return {
-        title: 'This is the title / ' + Date.now(),
-        body: 'This is the body / ' + Date.now()
-      }
-    },
-
   }
 }
 </script>
 
 <style>
-  #app.loading {
+  html, body, main {
+    height: 100%;
+  }
+
+  .loading {
     opacity: 0.5;
+  }
+
+  .section {
+    padding: 1.5em;
   }
 
   pre {
     border: 1px solid #EEE;
+    margin-bottom: 20px;
+  }
+
+  .control {
+    display: inline-block;
+  }
+
+  footer {
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+    text-align: right;
+  }
+
+  footer p {
+    display: inline-block;
+    padding: 6px 8px;
+    background: white;
   }
 </style>
