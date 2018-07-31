e4feb59a0470cdb498fb39230864607995814132
Dave Stewart <dave@davestewart.co.uk>
2018-07-31 02:47:29 +0100

    Fix demo logo

--- demo/src/app/views/pages/Home.vue
+++ demo/src/app/views/pages/Home.vue
@@ -2,11 +2,11 @@
 
   <div class="content home">
     <header class="banner">
-      <img src="../../../../../demo.old/src/core/assets/logo.png" width="350">
+      <img src="../../../core/assets/logo-image.png" width="350">
       <h1>Axios Actions</h1>
     </header>
 
-    <h4 class="title is-4">Running the examples</h4>
+    <h4>Running the examples</h4>
 
     <p>Use the menu on the left to navigate the examples.</p>
 
@@ -17,7 +17,7 @@
       <li>additional buttons link to any related documentation</li>
     </ul>
 
-    <h4 class="title is-4">Folder structure</h4>
+    <h4>Folder structure</h4>
 
     <p>The example code (for each feature or example) can be found in the <code>src/examples/</code> folder:</p>
     <pre>
@@ -29,7 +29,7 @@
     <p>You can manually navigate to the folders, or as mentioned above, use the edit buttons to jump directly to the
       file.</p>
 
-    <h4 class="title is-4">Using Vue Devtools</h4>
+    <h4>Using Vue Devtools</h4>
 
     <p>You will probably want to use Vue Devtools as you run the demo to see the store update.</p>
     <p>As CodeSandbox runs in an iframe, you'll need to open the preview pane in a new window. Click the arrow-like icon
