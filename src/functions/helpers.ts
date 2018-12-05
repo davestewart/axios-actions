0dc1d586a4b1d523e75977e6bec2aa9d8da86ddb
Dave Stewart <dave@davestewart.co.uk>
2018-12-05 15:41:32 +0000

    Add getUrls() helper

--- src/functions/helpers.ts
+++ src/functions/helpers.ts
@@ -1,4 +1,5 @@
 import { isObject } from '../utils/object'
+import ApiGroup from '../classes/ApiGroup'
 
 /**
  * Helper function to process single object or array
@@ -29,4 +30,45 @@ export function toJSON (data) {
   return isObject(data) && data.toJSON instanceof Function
     ? data.toJSON()
     : data
+}
+
+/**
+ * Helper function to get raw URLs from config or service
+ *
+ * @param instance
+ * @param root
+ */
+export function getUrls (instance: any, root = false)
+export function getUrls (instance: ApiGroup, root = false)
+export function getUrls (instance: any, root = false) {
+  // type
+  let isApiGroup = instance instanceof ApiGroup
+
+  // config
+  const config = isApiGroup
+    ? instance.actions.map
+    : instance
+
+  // root path
+  let rootPath = ''
+  if (root) {
+    if (!isApiGroup) {
+      throw new Error('Root path can only be queried on ApiGroup instances')
+    }
+    rootPath = instance.http.axios.defaults.baseURL || 'xx'
+  }
+
+  // build
+  return Object
+    .keys(config)
+    .reduce(function (urls, key) {
+      const value:any = config[key]
+      const path = typeof value === 'string'
+        ? value.replace(/^\w+\s+/, '')
+        : isApiGroup
+          ? value.config.url
+          : value.url
+      urls[key] = rootPath + path
+      return urls
+    }, {})
 }
